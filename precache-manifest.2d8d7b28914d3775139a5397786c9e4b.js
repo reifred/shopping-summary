d0fc483c8c71aab76018fe94ab0a29b0c0516637
reifred <reifred33@gmail.com>
2019-10-21 00:52:27 +0300

    Updates

---
+++ precache-manifest.2d8d7b28914d3775139a5397786c9e4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a69642b08ebc20c1e3337d03ebfa05e",
+    "url": "/shopping-summary/index.html"
+  },
+  {
+    "revision": "c0da0afa1dafb0e95ab1",
+    "url": "/shopping-summary/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "dc8ad2b9c1fa3bd07518",
+    "url": "/shopping-summary/static/css/main.61282fcc.chunk.css"
+  },
+  {
+    "revision": "c0da0afa1dafb0e95ab1",
+    "url": "/shopping-summary/static/js/2.734e4814.chunk.js"
+  },
+  {
+    "revision": "dc8ad2b9c1fa3bd07518",
+    "url": "/shopping-summary/static/js/main.97d69f93.chunk.js"
+  },
+  {
+    "revision": "942838fe115889dc3e28",
+    "url": "/shopping-summary/static/js/runtime-main.eb3b8efe.js"
+  }
+]);
